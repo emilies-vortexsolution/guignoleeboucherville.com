a6f47a307b4ffad45a53b025fbae65190a23153b
Émilie Sirotic <emilie.s@vortexsolution.com>
2020-11-17 13:58:13 -0500

    correction typos

--- inc/setup.php
+++ inc/setup.php
@@ -106,7 +106,7 @@ function display_sidebar() {
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100 );
 function assets() {
 
-  wp_enqueue_style( 'google_font', '//fonts.googleapis.com/css?family=Lato:400,700,900', false, null );
+  wp_enqueue_style( 'google_font', '//fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap', false, null );
 
   wp_enqueue_style( 'sage-css', Assets\asset_path( 'styles/main.css' ), false, null );
 
